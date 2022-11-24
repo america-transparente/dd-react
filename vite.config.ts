e25aacf4b7eeb6d3a4b025c39bf907d1cc338d1b
Agustín Covarrubias <agucova@uc.cl>
2022-11-24 00:03:14 -0300

    dev: add bundle analyzer

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
+import { defineConfig, type PluginOption } from 'vite'
 import react from '@vitejs/plugin-react'
+import { visualizer } from "rollup-plugin-visualizer";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react() as PluginOption, visualizer() as PluginOption],
 })
